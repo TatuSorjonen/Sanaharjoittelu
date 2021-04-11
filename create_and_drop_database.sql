db485771e68e2e278e9aba2059c363afe8ecaf6f
Tatu Sorjonen <tatu.sorjonen@gmail.com>
2021-04-11 20:10:50 +0300

    Commitoitu

--- create_and_drop_database.sql
+++ create_and_drop_database.sql
@@ -1,4 +1,7 @@
+DROP TABLE words;
+DROP TABLE deck;
 DROP TABLE dictionary_users;
+
 CREATE TABLE dictionary_users (
 	user_id SERIAL PRIMARY KEY,
 	username VARCHAR (20) UNIQUE NOT NULL,
@@ -6,10 +9,18 @@ CREATE TABLE dictionary_users (
 	teacher INTEGER NOT NULL
 );
 
+CREATE TABLE deck (
+        deck_id SERIAL PRIMARY KEY,
+        difficulty INTEGER,
+        name VARCHAR (20),
+        user_id INTEGER REFERENCES dictionary_users
+);
+
 CREATE TABLE words (
 	card_id SERIAL PRIMARY KEY,
+	--deck_id INTEGER REFERENCES deck NOT NULL,
 	word VARCHAR (50) NOT NULL,
-	translation VARCHAR (50) NOT NULL
+	translation VARCHAR (50) NOT NULL	
 );
 
 
